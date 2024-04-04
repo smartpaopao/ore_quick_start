a167916e192d7bfafd1ce6aa87de9de8d5b7d140
smartpaopao <104874644+smartpaopao@users.noreply.github.com>
2024-04-05 00:03:09 +0800

    Create ore_tmux.sh

---
+++ ore_tmux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 私钥作为第一个参数传入
+PrivateKey=$1
+
+# ID 的替换值作为第二个参数传入
+ID=$2
+
+# 使用传入的私钥写入到 /home/ubuntu/.config/solana/id.json
+echo "$PrivateKey" > /home/ubuntu/.config/solana/$ID.json
+
+# 修改环境变量，将 Solana的二进制文件路径加入到PATH中
+export PATH="/home/ubuntu/.local/share/solana/install/active_release/bin:$PATH"
+
+# 给ore脚本执行权限
+chmod +x ore
+
+# 再次执行 ore 挖矿命令
+while true
+do 
+   ./ore --rpc https://solana-mainnet.phantom.app/YBPpkkN4g91xDiAnTE9r0RcMkjg0sKUIWvAfoFVJ --keypair ~/.config/solana/$ID.json --priority-fee 100000 mine --threads 30
+done
