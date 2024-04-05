79544ec5463030dea7efca9b7a209d14bb806f9e
smartpaopao <104874644+smartpaopao@users.noreply.github.com>
2024-04-06 01:18:39 +0800

    Update ore_tmux.sh

--- ore_tmux.sh
+++ ore_tmux.sh
@@ -18,5 +18,5 @@ chmod +x ore
 # 再次执行 ore 挖矿命令
 while true
 do 
-   ./ore --rpc https://nameless-blue-pond.solana-mainnet.quiknode.pro/a1c3f257d8e2870a9fb166c12e8bf4e028a781c1 --keypair ~/.config/solana/$ID.json --priority-fee 100000 mine --threads 30
+   ./ore --rpc https://nameless-blue-pond.solana-mainnet.quiknode.pro/a1c3f257d8e2870a9fb166c12e8bf4e028a781c1 --keypair ~/.config/solana/$ID.json --priority-fee 500000 mine --threads 30
 done
